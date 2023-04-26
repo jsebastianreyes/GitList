c2265da98b2c4af1e453dfb3b73ecdac11889e7b
Sebastian Reyes <juansreyes.salinas@gmail.com>
2023-04-26 14:31:19 -0500

    ajuste filtros

--- src/components/repo-list.js
+++ src/components/repo-list.js
@@ -20,20 +20,21 @@ function RepoList({ repoList, search, selectType, language, sort }) {
       const data = list.filter((item) => { 
       return item.name.search(search) >= 0  &&
       (language ===  'all' || item.language === language) && 
-      (selectType === 'all' || item.forks_count >= 0)  
+      (selectType === 'all' || item.forks_count >= 1)  
     })
 
       if(selectType !== 'all' && sort === 'updated') data.sort((a,b) => b.forks_count - a.forks_count )
       else if(sort === 'name' && selectType === 'all') data.sort((a, b) => a.name.localeCompare(b.name))
-
+      else data.sort((a, b) => a.name.localeCompare(b.name))
       busquedas = data.length 
       list = data
+      
     }
 
             
     return (
       <RepoListStyled>
-        
+         
         {busquedas === 0 ? <NotFound/> : null}    
       {list.map((item) => {
         return <RepoItem {...item} key={item.id} />
